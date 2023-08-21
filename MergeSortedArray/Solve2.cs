3d752bfda22e8b899f22ebe7f40bf979fc2a36a7
arbythsursy4361 <42234962+arbythsursy4361@users.noreply.github.com>
2023-08-21 11:45:40 +0800

    Update Solve2.cs

--- MergeSortedArray/Solve2.cs
+++ MergeSortedArray/Solve2.cs
@@ -1,7 +1,6 @@
 public class Solution {
     public void Merge(int[] nums1, int m, int[] nums2, int n) {        
         int position = m + n - 1, p1 = m - 1, p2 = n - 1;
-
         while(p2 >= 0){
             if(p1 >= 0 && nums1[p1] > nums2[p2]){
                 nums1[position--] = nums1[p1--];
