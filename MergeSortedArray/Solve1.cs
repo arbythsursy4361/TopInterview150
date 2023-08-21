2b6c5258c43aa6493f4765fae9036790efe3bed0
arbythsursy4361 <42234962+arbythsursy4361@users.noreply.github.com>
2023-08-21 11:48:15 +0800

    Update Solve1.cs

--- MergeSortedArray/Solve1.cs
+++ MergeSortedArray/Solve1.cs
@@ -4,34 +4,22 @@ public class Solution {
         int[] result = new int[m + n];
         while(flag1 < m && flag2 < n){
             if(nums1[flag1] < nums2[flag2]){
-                result[count] = nums1[flag1];
-                flag1++;
-                count++;
+                result[count++] = nums1[flag1++];
             }
             else if(nums2[flag2] < nums1[flag1]){
-                result[count] = nums2[flag2];
-                flag2++;
-                count++;
+                result[count++] = nums2[flag2++];
             }
             else{
-                result[count] = nums1[flag1];
-                flag1++;
-                count++;
-                result[count] = nums2[flag2];
-                flag2++;
-                count++;
+                result[count++] = nums1[flag1++];
+                result[count++] = nums2[flag2++];
             }
         }
 
         while(flag1 < m){
-            result[count] = nums1[flag1];
-            flag1++;
-            count++;
+            result[count++] = nums1[flag1++];
         }
         while(flag2 < n){
-            result[count] = nums2[flag2];
-            flag2++;
-            count++;
+            result[count++] = nums2[flag2++];
         }
 
         for(int i = 0; i < result.Length; i++){
